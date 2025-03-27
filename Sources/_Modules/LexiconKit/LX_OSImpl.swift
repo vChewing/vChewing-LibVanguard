a1e096637087db7d4e8b0f3f5877d65a86ef0c63
ShikiSuen <shikisuen@outlook.com>
2025-03-27 14:34:32 +0800

    LexiconKit // Use ArrayBuilder DSL to handle query results.

--- Sources/_Modules/LexiconKit/LX_OSImpl.swift
+++ Sources/_Modules/LexiconKit/LX_OSImpl.swift
@@ -31,3 +31,36 @@ internal func isAppleSilicon() -> Bool {
     return false
   #endif
 }
+
+// MARK: - ArrayBuilder
+
+@resultBuilder
+enum ArrayBuilder<Element> {
+  static func buildEither(first elements: [Element]) -> [Element] {
+    elements
+  }
+
+  static func buildEither(second elements: [Element]) -> [Element] {
+    elements
+  }
+
+  static func buildOptional(_ elements: [Element]?) -> [Element] {
+    elements ?? []
+  }
+
+  static func buildExpression(_ expression: Element) -> [Element] {
+    [expression]
+  }
+
+  static func buildExpression(_: ()) -> [Element] {
+    []
+  }
+
+  static func buildBlock(_ elements: [Element]...) -> [Element] {
+    elements.flatMap { $0 }
+  }
+
+  static func buildArray(_ elements: [[Element]]) -> [Element] {
+    Array(elements.joined())
+  }
+}
