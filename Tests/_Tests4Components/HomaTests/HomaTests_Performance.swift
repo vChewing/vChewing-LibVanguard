e01c8cf71958e4526b8d7a623bde38b1963f87f3
ShikiSuen <shikisuen@outlook.com>
2025-09-09 03:50:23 +0800

    Update HomaTests_Performance.swift

--- Tests/_Tests4Components/HomaTests/HomaTests_Performance.swift
+++ Tests/_Tests4Components/HomaTests/HomaTests_Performance.swift
@@ -209,10 +209,10 @@ public struct HomaPerformanceTests: HomaTestSuite {
     // 建立真實的中文拼音輸入模式 - 使用與 Mock 資料對應的拼音讀音
     let knownPinyin = [
       "suo3", "wei4", "kai1", "tuo4", "jiu4", "shi4",
-      "yan2", "zhe5", "qian2", "ren2", "jin4", "de5",
-      "dao4", "lu4", "zou3", "chu1", "geng1", "yao2",
-      "yuan3", "ju4", "li2", "yin1", "wei2", "cong2",
-      "lai2", "bu4", "you2", "ta1", "pu1",
+      "yan2", "zhe5", "qian2", "ren2", "wei4", "jin4",
+      "de5", "dao4", "lu4", "zou3", "chu1", "geng1",
+      "yao2", "yuan3", "ju4", "li2", "yin1", "wei2",
+      "cong2", "lai2", "bu4", "you2", "ta1", "pu1",
     ]
 
     // 建立一些雙元圖組合
